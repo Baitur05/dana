5747fda63fad8c4cfb5b3deb77c0b5567defba47
bore <ibrakhimov2005@gmail.com>
2023-02-03 17:41:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./component/Auth/Login";
+import Register from "./component/Auth/Register";
+import RegisterSuccess from "./component/Auth/RegisterSuccess";
+import NavScrollExample from "./component/Navbar/Navbar";
+import Home from "./Home/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavScrollExample />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register-success" element={<RegisterSuccess />} />
+        <Route path="*" element={<h1>NON FOUND PAGE</h1>} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
